e752e20a91334e8a35682914a2b7b601b62fc4a2
David Durst <davidbdurst@gmail.com>
2019-02-15 21:20:21 -0800

    added bigger examples, split out examples from magmacompoiler

--- src/StencilExamples.hs
+++ src/StencilExamples.hs
@@ -94,75 +94,83 @@ thirdConv = downsampleLB4x4 >>>
   divBy4 lengthProxy
   where
     lengthProxy = Proxy @4
-{-
--- downsampleLB8x8 >>> scalableConvConstsForLB lengthProxy >>>
-xxz = (downsampleLB8x8 >***<
-        (produceRightUnitMix (Proxy :: Proxy (TSeq 64 0 (Atom ()))) >>>
-         scalableConvConsts (Proxy @16) (Proxy @4)))
-secondConv = seq_to_tseqC (iterC fullLengthProxy addUnitType) >>>
-  (downsampleLB8x8 >***< (produceRightUnitMix (Proxy :: Proxy (TSeq 64 0 (Atom ()))) >>>
-                           scalableConvConsts underutilLengthProxy underutilMult)) >>>
-  scalableConvMuls underutilLengthProxy underutilMult >>>
-  scalableNTuplesToFlatSSeq underutilLengthProxy underutilMult >>>
-  scalableConvFold underutilLengthProxy underutilMult >>>
-  divBy4 underutilLengthProxy underutilMult
-  where
-    fullLengthProxy = Proxy @64
-    underutilLengthProxy = Proxy @16
-    underutilMult = Proxy @4
 
-thirdConv = seq_to_tseqC (iterC fullLengthProxy addUnitType) >>>
-  (downsampleLB4x4 >***< (produceRightUnitMix (Proxy :: Proxy (TSeq 16 0 (Atom ()))) >>>
-                           scalableConvConsts underutilLengthProxy underutilMult)) >>>
-  scalableConvMuls underutilLengthProxy underutilMult >>>
-  scalableNTuplesToFlatSSeq underutilLengthProxy underutilMult >>>
-  scalableConvFold underutilLengthProxy underutilMult >>>
-  divBy4 underutilLengthProxy underutilMult
-  where
-    fullLengthProxy = Proxy @16
-    underutilLengthProxy = Proxy @4
-    underutilMult = Proxy @4
-
-
--- later stencils are underutiled as they are only going to take in 1 input every nth
--- the second every 4th (as it is a 4x4 image that runs for as many clocks as the 8x8)
--- the third every 16th (as it is a 2x2 image that runs for as many clocks as the 8x8)
-downsampleStencilChain = firstConv >>>
-  (underutilC (Proxy @4) $ seq_to_tseqC $ iterC (Proxy @64) reshapeImplicitC) >>>
-  underutilC (Proxy @4) secondConv >>>
-  (underutilC (Proxy @16) $ seq_to_tseqC $ iterC (Proxy @16) reshapeImplicitC) >>>
-  underutilC (Proxy @16) thirdConv
-{-
-secondConv = iterC lengthProxy addUnitType >>>
-  (downsampleLB4x4 >***< scalableConvConsts lengthProxy) >>>
-  scalableConvMuls lengthProxy >>> scalableNTuplesToFlatSSeq lengthProxy >>>
-  scalableConvFold lengthProxy
-  where
-    lengthProxy = Proxy @4
-
-thirdConv = iterC lengthProxy addUnitType >>>
-  (downsampleLB2x2 >***< scalableConvConsts lengthProxy) >>>
-  scalableConvMuls lengthProxy >>> scalableNTuplesToFlatSSeq lengthProxy >>>
-  scalableConvFold lengthProxy
-  where
-    lengthProxy = Proxy @1
-
--}
-downsampleStencilChain =
-  seq_to_sseqC firstConv >>> reshapeImplicitC >>>
-  seq_to_sseqC secondConv >>> reshapeImplicitC >>>
-  seq_to_sseqC thirdConv
-
--}
 downsampleStencilChain = firstConv >>> secondConv >>> thirdConv
 downsampleStencilChain1Per64 = seq_to_tseqC downsampleStencilChain
 downsampleStencilChain1Per32 = seq_to_tseqC $ split_seq_to_sseqC (Proxy @2) downsampleStencilChain
+
+makeConv downsample lengthProxy = downsample >>>
+  tupleLBOutWithConvConsts lengthProxy downsample >>>
+  convMuls lengthProxy >>>
+  flattenNTuples lengthProxy >>>
+  convFold lengthProxy >>>
+  foldOutputToDivInput lengthProxy >>>
+  divBy4 lengthProxy
+
+-- bigger downsample examples
+-- this will allow 16, 32, and 64 in per clock so can do 1, 2, and 4 px out per clock
+-- in per clock determine out per clock. 256x256 ensures need real amount of ram
+-- for rowbuffer as 64 is 1/4 rather than 1/2 of row size
+lb_256x256Im_2x2Win_2x2Downsample = (lineBuffer (Proxy :: Proxy (Atom Int))
+                       (Proxy @2) (Proxy @2) (Proxy @256) (Proxy @256)
+                       (Proxy @2) (Proxy @2) (Proxy @0) (Proxy @0))
+lb_256x256Im_2x2Win_2x2DownsampleCData = buildCompilationData lb_256x256Im_2x2Win_2x2Downsample
+lb_128x128Im_2x2Win_2x2Downsample = (lineBuffer (Proxy :: Proxy (Atom Int))
+                       (Proxy @2) (Proxy @2) (Proxy @128) (Proxy @128)
+                       (Proxy @2) (Proxy @2) (Proxy @0) (Proxy @0))
+lb_128x128Im_2x2Win_2x2DownsampleCData = buildCompilationData lb_128x128Im_2x2Win_2x2Downsample
+lb_64x64Im_2x2Win_2x2Downsample = (lineBuffer (Proxy :: Proxy (Atom Int))
+                       (Proxy @2) (Proxy @2) (Proxy @64) (Proxy @64)
+                       (Proxy @2) (Proxy @2) (Proxy @0) (Proxy @0))
+lb_64x64Im_2x2Win_2x2DownsampleCData = buildCompilationData lb_64x64Im_2x2Win_2x2Downsample
+
+conv_256x256 = makeConv lb_256x256Im_2x2Win_2x2Downsample (Proxy @16384)
+conv_128x128 = makeConv lb_128x128Im_2x2Win_2x2Downsample (Proxy @4096)
+conv_64x64 = makeConv lb_64x64Im_2x2Win_2x2Downsample (Proxy @1024)
+--second_256x256_conv = makeConv lb_256x256Im_2x2Win_2x2Downsample
+
+unscheduled_downsample_256x256_to_32x32 = conv_256x256 >>> conv_128x128 >>> conv_64x64
+downsample_256x256_to_32x32_1px_in_per_clk = seq_to_tseqC unscheduled_downsample_256x256_to_32x32
+downsample_256x256_to_32x32_2px_in_per_clk = seq_to_tseqC $ split_seq_to_sseqC (Proxy @2) unscheduled_downsample_256x256_to_32x32
+downsample_256x256_to_32x32_4px_in_per_clk = seq_to_tseqC $ split_seq_to_sseqC (Proxy @4) unscheduled_downsample_256x256_to_32x32
+downsample_256x256_to_32x32_8px_in_per_clk = seq_to_tseqC $ split_seq_to_sseqC (Proxy @8) unscheduled_downsample_256x256_to_32x32
+downsample_256x256_to_32x32_16px_in_per_clk = seq_to_tseqC $ split_seq_to_sseqC (Proxy @16) unscheduled_downsample_256x256_to_32x32
+downsample_256x256_to_32x32_32px_in_per_clk = seq_to_tseqC $ split_seq_to_sseqC (Proxy @32) unscheduled_downsample_256x256_to_32x32
+downsample_256x256_to_32x32_64px_in_per_clk = seq_to_tseqC $ split_seq_to_sseqC (Proxy @64) unscheduled_downsample_256x256_to_32x32
+{-
+downsampleStencilChain1Per64 = seq_to_tseqC downsampleStencilChain
+downsampleStencilChain1Per32 = seq_to_tseqC $ split_seq_to_sseqC (Proxy @2) downsampleStencilChain
+-}
 --increaseUtilTtoSC (Proxy @2) downsampleStencilChain1Per64
 writeAllStencils compileToVerilog = do
   let dirToWrite = if compileToVerilog then "pyExamples/verilog/" else "pyExamples/simulation/"
   writeProgramToFile "downsampleStencilChain1Per64" preludeLocationStrForEx epilogueLocationStrForEx
     (dirToWrite ++ "downsampleStencilChain1Per64.py") compileToVerilog downsampleStencilChain1Per64 --firstConv --(divBy4 (Proxy @64) (Proxy @4))-- firstConv --downsampleStencilChain1Per64
-  writeProgramToFile "downsampleStencilChain1Per32" preludeLocationStrForEx epilogueLocationStrForEx
-    (dirToWrite ++ "downsampleStencilChain1Per32.py") compileToVerilog downsampleStencilChain1Per32
-
-
+  writeProgramToFile "downsample_256x256_to_32x32_1px_in_per_clk"
+    preludeLocationStrForEx epilogueLocationStrForEx
+    (dirToWrite ++ "downsample_256x256_to_32x32_1px_in_per_clk.py")
+    compileToVerilog downsample_256x256_to_32x32_1px_in_per_clk
+  writeProgramToFile "downsample_256x256_to_32x32_2px_in_per_clk"
+    preludeLocationStrForEx epilogueLocationStrForEx
+    (dirToWrite ++ "downsample_256x256_to_32x32_2px_in_per_clk.py")
+    compileToVerilog downsample_256x256_to_32x32_2px_in_per_clk
+  writeProgramToFile "downsample_256x256_to_32x32_4px_in_per_clk"
+    preludeLocationStrForEx epilogueLocationStrForEx
+    (dirToWrite ++ "downsample_256x256_to_32x32_4px_in_per_clk.py")
+    compileToVerilog downsample_256x256_to_32x32_4px_in_per_clk
+  writeProgramToFile "downsample_256x256_to_32x32_8px_in_per_clk"
+    preludeLocationStrForEx epilogueLocationStrForEx
+    (dirToWrite ++ "downsample_256x256_to_32x32_8px_in_per_clk.py")
+    compileToVerilog downsample_256x256_to_32x32_8px_in_per_clk
+  writeProgramToFile "downsample_256x256_to_32x32_16px_in_per_clk"
+    preludeLocationStrForEx epilogueLocationStrForEx
+    (dirToWrite ++ "downsample_256x256_to_32x32_16px_in_per_clk.py")
+    compileToVerilog downsample_256x256_to_32x32_16px_in_per_clk
+  writeProgramToFile "downsample_256x256_to_32x32_32px_in_per_clk"
+    preludeLocationStrForEx epilogueLocationStrForEx
+    (dirToWrite ++ "downsample_256x256_to_32x32_32px_in_per_clk.py")
+    compileToVerilog downsample_256x256_to_32x32_32px_in_per_clk
+  writeProgramToFile "downsample_256x256_to_32x32_64px_in_per_clk"
+    preludeLocationStrForEx epilogueLocationStrForEx
+    (dirToWrite ++ "downsample_256x256_to_32x32_64px_in_per_clk.py")
+    compileToVerilog downsample_256x256_to_32x32_64px_in_per_clk
