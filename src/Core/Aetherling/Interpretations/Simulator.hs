ef94022b099efe2bdf40c2d11e50abde41d99a25
David Durst <davidbdurst@gmail.com>
2019-03-23 17:00:06 -0700

    implemented simulator

--- src/Core/Aetherling/Interpretations/Simulator.hs
+++ src/Core/Aetherling/Interpretations/Simulator.hs
@@ -22,17 +22,17 @@ instance Sequence_Language Simulation_Env where
   notC _ = fail $ fail_message "absC" "Atom_Bit"
 
   -- binary operators
-  addC (Atom_Int x) (Atom_Int y) = return $ Atom_Int $ x+y
-  addC _ _ = fail $ fail_message "addC" "Atom_Int's"
+  addC (Atom_Tuple (Atom_Int x, Atom_Int y)) = return $ Atom_Int $ x+y
+  addC _ = fail $ fail_message "addC" "Atom_Int's"
 
-  eqC x y = return $ Atom_Bit $ x == y 
+  eqC (Atom_Tuple (x,y)) = return $ Atom_Bit $ x == y 
 
   -- generators
   lut_genC xs (Atom_Int i) | i < length xs = return $ xs !! i 
   lut_genC xs (Atom_Int i) = fail "lut lookup index out of bounds"
   lut_genC _ _ = fail $ fail_message "lut_genC" "Atom_Int"
 
-  const_genC x = return x
+  const_genC x _ = return x
 
   -- sequence operators
   up_1dC proxyN (Seq elem) = return $ Seq $
@@ -55,26 +55,7 @@ instance Sequence_Language Simulation_Env where
     Proxy n -> (a -> m b) -> (Seq n a -> m (Seq n b))
 
  -} 
-  mapC proxyN f |
-    typeOf f == typeOf Atom_Unit || 
-    typeOf f == typeOf (Atom_Bit True) ||
-    typeOf f == typeOf (Atom_Int 1) =
-    -- I need to say that Seq n a is the same as
-    -- Type_Lifted_To_Seq n a. I know this as I'm only
-    -- allowing things that are not functions in here.
-    return $ unsafeCoerce $ Seq $ V.replicate' proxyN f
-  -- only do following on functions
-  {-
-  mapC proxyN f |
-    is_function f =
-    return $ unsafeCoerce $ seq_all_args vector_of_fs
-    where
-      f_as_function :: 
-      vector_of_fs = V.replicate' proxyN f
-      is_function f = L.isInfixOf "->" $ show $ typeOf f
-      seq_all_args f | is_function f =
-                       V.zipWith ($)
--}
+  mapC _ f input = return $ fmap f input
 
   -- composition operators
   (>>>) f g x = f x >>= g
