eecd0775eb4f64622b3c7affb56d7994284c7405
David Durst <davidbdurst@gmail.com>
2019-04-21 15:51:47 -0700

    getting next slowdown now on sequence ast

--- src/Core/Aetherling/Interpretations/Valid_Slowdowns.hs
+++ src/Core/Aetherling/Interpretations/Valid_Slowdowns.hs
@@ -1,10 +1,10 @@
 module Aetherling.Interpretations.Valid_Slowdowns where
-import Aetherling.ASTs.Space_Time
+import Aetherling.ASTs.Sequence
 import Aetherling.Types.Declarations
 import Data.Map
 import Data.Maybe
 
-get_next_valid_slowdown :: ST_DAG -> Int -> Int
+get_next_valid_slowdown :: Seq_DAG -> Int -> Int
 get_next_valid_slowdown dag@(DAG nodes _) factor = do
   let is_cur_factor_valid = and $ fmap (is_slowdown_valid factor) nodes
   if is_cur_factor_valid
@@ -13,7 +13,7 @@ get_next_valid_slowdown dag@(DAG nodes _) factor = do
 
 -- check if this slowdown amount is valid
 -- always slowing down parallel _s operators, so don't worry about _t
-is_slowdown_valid :: Int -> Space_Time_Language_AST -> Bool
+is_slowdown_valid :: Int -> Sequence_Language_AST -> Bool
 -- these don't impact ability to slow down
 is_slowdown_valid _ IdN = True
 is_slowdown_valid _ AbsN = True
@@ -26,24 +26,16 @@ is_slowdown_valid _ (Lut_GenN _) = True
 is_slowdown_valid _ (Const_GenN _) = True
 
 -- sequence operators
-is_slowdown_valid factor (Up_1d_sN n _) = factor_valid n factor
-is_slowdown_valid _ (Up_1d_tN n _ _) = undefined 
-is_slowdown_valid factor (Down_1d_sN n _) = factor_valid n factor
-is_slowdown_valid _ (Down_1d_tN _ _ _) = undefined
-is_slowdown_valid factor (Partition_ssN no ni _) =
+is_slowdown_valid factor (Up_1dN n _) = factor_valid n factor
+is_slowdown_valid factor (Down_1dN n _) = factor_valid n factor
+is_slowdown_valid factor (PartitionN no ni _) =
   factor_valid no factor || factor_valid (no*ni) factor
-is_slowdown_valid factor (Unpartition_ssN no ni _) =
+is_slowdown_valid factor (UnpartitionN no ni _) =
   factor_valid no factor || factor_valid (no*ni) factor
-is_slowdown_valid _ (Partition_ttN _ _ _ _ _) = undefined
-is_slowdown_valid _ (Unpartition_ttN _ _ _ _ _) = undefined
-is_slowdown_valid _ (Partition_tsN _ _ _ _) = undefined
-is_slowdown_valid _ (Unpartition_tsN _ _ _ _) = undefined 
-is_slowdown_valid factor (Map_sN n f) =
+is_slowdown_valid factor (MapN n f) =
   factor_valid n factor || (get_next_valid_slowdown f factor == factor)
-is_slowdown_valid _ (Map_tN _ _ _) = undefined
-is_slowdown_valid factor (Map2_sN n f) =
+is_slowdown_valid factor (Map2N n f) =
   factor_valid n factor || (get_next_valid_slowdown f factor == factor)
-is_slowdown_valid _ (Map2_tN _ _ _) = undefined
 
 is_slowdown_valid _ (FstN _ _) = True
 is_slowdown_valid _ (SndN _ _) = True
