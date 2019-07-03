3d27f43eaf93af7d0cdd0a63f00fe9796218261b
David Durst <davidbdurst@gmail.com>
2019-07-03 10:47:28 -0700

    down_2dc passes type checker

--- src/AppMains/Blur/Main.hs
+++ src/AppMains/Blur/Main.hs
@@ -16,20 +16,22 @@ down_2d_mC down_row down_col in_col idx_row idx_col x = do
 
 simp l = mapC l absC >>> mapC l absC
 
-down_2dC down_row down_col in_col in_row idx_row idx_col =
-  partitionC down_2d_one_row_repeats (down_row `mul_p` down_col `mul_p` in_col) >>>
-  mapC down_2d_one_row_repeats (down_2d_one_rowC down_row down_col in_col idx_row idx_col) 
-  --unpartitionC down_2d_one_row_repeats (Proxy @1)
+down_2dC down_row down_col in_row in_col idx_row idx_col =
+  partitionC out_row (down_row `mul_p` down_col `mul_p` out_col) >>>
+  mapC out_row (down_2d_one_rowC down_row down_col out_col idx_row idx_col) >>>
+  unpartitionC out_row out_col
   where
-    down_2d_one_row_repeats = in_row `div_p` down_row
+    out_row = in_row `div_p` down_row
+    out_col = in_col `div_p` down_col
 
-down_2d_one_rowC down_row down_col in_col idx_row idx_col =
-  partitionC (down_col `mul_p` in_col) down_col >>>
-  (mapC (down_row `mul_p` in_col) (down_1dC down_col idx_col)) >>>
-  unpartitionC (down_row `mul_p` in_col) (Proxy @1) >>>
-  partitionC down_row in_col >>>
-  down_1dC down_row idx_col >>>
-  unpartitionC (Proxy @1) in_col
+
+down_2d_one_rowC down_row down_col out_col idx_row idx_col =
+  partitionC (down_row `mul_p` out_col) down_col >>>
+  (mapC (down_row `mul_p` out_col) (down_1dC down_col idx_col)) >>>
+  unpartitionC (down_row `mul_p` out_col) (Proxy @1) >>>
+  partitionC down_row out_col >>>
+  down_1dC down_row idx_row >>>
+  unpartitionC (Proxy @1) out_col
   --where
    -- out_col = total_col `div_p` down_col
   
@@ -44,13 +46,13 @@ mul_p :: forall n m . (KnownNat n, KnownNat m) =>
   Proxy n -> Proxy m -> Proxy (n GHC.TypeLits.* m)
 mul_p _ _ = Proxy :: Proxy (n GHC.TypeLits.* m)
 
-div_p :: forall n m . (KnownNat n, KnownNat m) =>
-  Proxy n -> Proxy (n GHC.TypeLits.* m) -> Proxy m
-div_p _ _ = Proxy :: Proxy m
-
 div_p' :: forall n m . (KnownNat n, KnownNat m) =>
+  Proxy (n GHC.TypeLits.* m) -> Proxy n -> Proxy m
+div_p' _ _ = Proxy :: Proxy m
+
+div_p :: forall n m . (KnownNat n, KnownNat m) =>
   Proxy n -> Proxy m -> Proxy (GHC.TypeLits.Div n m)
-div_p' _ _ = Proxy :: Proxy (GHC.TypeLits.Div n m)
+div_p _ _ = Proxy :: Proxy (GHC.TypeLits.Div n m)
    {-
 down_1dC :: (KnownNat n, KnownNat i,
               Aetherling_Value a) =>
