47d59080c0aba9a75388120291c77724da970268
David Durst <davidbdurst@gmail.com>
2019-08-25 15:59:10 -0700

    removed coprime issue, adding more facts about indexes

--- src/Core/Aetherling/Functional_Models/Flip_ts_to_st.hs
+++ src/Core/Aetherling/Functional_Models/Flip_ts_to_st.hs
@@ -40,7 +40,7 @@ st_to_ts s_len t_len = do
   let st = [[ x*t_len + t | t <- [0 .. t_len - 1]] | x <- [0 .. s_len - 1]]
   let st_banked_data = add_buffer_data_for_st st
   let banks = reshape_to_banks st_banked_data
-  --traceM $ show $ fmap (fmap banked_elem) banks
+  traceM $ show $ fmap (fmap banked_elem) banks
   -- this verifies that each bank is never written to on the same clock
   let bank_write_conflict = any id $
         map has_duplicates $ map (map in_t_idx) banks
@@ -72,15 +72,6 @@ coprime x y = do
 -- | given an [[Int]] represent an outer SSeq len and an inner TSeq len
 -- return the same structure where bank and indexes are assigned to each value
 add_buffer_data_for_st :: [[Int]] -> [[Banked_Value]]
-add_buffer_data_for_st st | coprime t_len s_len =
-  [
-    [ Banked_Value (st !! x !! t) t x
-      (flat_idx x t `mod` s_len) (flat_idx x t `div` s_len)
-    | t <- [0..(t_len - 1)]] | x <- [0..(s_len - 1)]]
-  where
-    flat_idx x t = t_len * x + t
-    s_len = length st
-    t_len = length (st !! 0)
 add_buffer_data_for_st st =
   [
     [ Banked_Value (st !! x !! t) t x
@@ -95,14 +86,6 @@ add_buffer_data_for_st st =
 -- | given an [[Banked_Value]] representing a set of banks of memory
 -- return the same structure in the order of the TSeq (SSeq) read from the bank
 read_data_from_banks_for_st :: [[Banked_Value]] -> [[Banked_Value]]
-read_data_from_banks_for_st banks | coprime t_len s_len =
-  [
-    [ banks !! (flat_idx x t `mod` s_len) !! (flat_idx x t `div` s_len)
-    | t <- [0..(t_len - 1)]] | x <- [0..(s_len - 1)]]
-  where
-    flat_idx x t = t_len * x + t
-    s_len = length banks
-    t_len = length (banks !! 0)
 read_data_from_banks_for_st banks =
   [
     [ banks !!
@@ -117,13 +100,6 @@ read_data_from_banks_for_st banks =
 -- | given an [[Int]] represent an outer TSeq len and an inner SSeq len
 -- return the same structure where bank and indexes are assigned to each value
 add_buffer_data_for_ts :: [[Int]] -> [[Banked_Value]]
-add_buffer_data_for_ts ts | coprime t_len s_len =
-  [
-    [ Banked_Value (ts !! t !! x) t x x t
-    | x <- [0..(s_len - 1)]] | t <- [0..(t_len - 1)]]
-  where
-    t_len = length ts
-    s_len = length (ts !! 0)
 add_buffer_data_for_ts ts =
   [
     [ Banked_Value (ts !! t !! x) t x ((x + (flat_idx x t `div` lcm t_len s_len)) `mod` s_len) t
@@ -136,13 +112,6 @@ add_buffer_data_for_ts ts =
 -- | given an [[Banked_Value]] representing a set of banks of memory
 -- return the same structure in the order of the TSeq (SSeq) read from the bank
 read_data_from_banks_for_ts :: [[Banked_Value]] -> [[Banked_Value]]
-read_data_from_banks_for_ts banks | coprime t_len s_len =
-  [
-    [ banks !! x !! t
-    | x <- [0..(s_len - 1)]] | t <- [0..(t_len - 1)]]
-  where
-    s_len = length banks
-    t_len = length (banks !! 0)
 read_data_from_banks_for_ts banks =
   [
     [ banks !! ((x + (flat_idx x t `div` lcm t_len s_len)) `mod` s_len) !! t
