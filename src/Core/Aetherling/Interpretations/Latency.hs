cea0b2f8185445376294e8ade0b9007fd046de79
David Durst <davidbdurst@gmail.com>
2019-09-14 15:06:27 -0700

    adding latnecy imports

--- src/Core/Aetherling/Interpretations/Latency.hs
+++ src/Core/Aetherling/Interpretations/Latency.hs
@@ -1,1 +1,8 @@
 module Aetherling.Interpretations.Latency where
+import Aetherling.Languages.Space_Time.Deep.Types
+import Aetherling.Languages.Space_Time.Deep.Expr
+import Aetherling.Languages.Space_Time.Deep.Expr_Builders
+
+compute_latency :: Expr -> IO Int
+compute_latency (ReshapeN in_t out_t _ _) = undefined
+
