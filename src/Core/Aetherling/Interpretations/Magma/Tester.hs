6eacc6d9896f5753bb8f7d6e224e86121249bfe2
David Durst <davidbdurst@gmail.com>
2019-09-19 11:46:11 -0700

    fixed generating flat_idxs from type

--- src/Core/Aetherling/Interpretations/Magma/Tester.hs
+++ src/Core/Aetherling/Interpretations/Magma/Tester.hs
@@ -29,66 +29,70 @@ generate_fault_inputs_for_st_type :: AST_Type -> Flat_Idx_To_Magma_Val -> String
 generate_fault_inputs_for_st_type t conv_func = do
   let total_width = num_atoms_t t
   let total_time = clocks_t t
+  let valid_time = valid_clocks_t t
   let flat_results =
-        generate_fault_inputs_for_st_type' t conv_func total_width total_time 0 0 True 0
+        generate_fault_inputs_for_st_type' t conv_func total_width total_time
+        valid_time 0 0 True 0
   let sorted_results = sortBy (\x y -> compare (flat_t x) (flat_t y)) flat_results
   let grouped_by_t = groupBy (\x y -> flat_t x == flat_t y) sorted_results
   let grouped_and_sorted_by_s =
         map (sortBy (\x y -> compare (flat_s x) (flat_s y))) grouped_by_t
   let invs_filtered = filter (mv_valid . head) grouped_and_sorted_by_s
-  let grouped_and_sorted_just_vals = map (map magma_value) invs_filtered
+  let grouped_and_sorted_just_vals = map (map magma_value) grouped_and_sorted_by_s
   -- need to filter out quotes from string printing
-  filter (\x -> x /= '\"') $ show invs_filtered--grouped_and_sorted_just_vals
+  filter (\x -> x /= '\"') $ show grouped_and_sorted_just_vals
 
 generate_fault_inputs_for_st_type' :: AST_Type -> Flat_Idx_To_Magma_Val ->
-                                      Int -> Int -> Int -> Int -> Bool ->
+                                      Int -> Int -> Int -> Int -> Int -> Bool ->
                                       Int -> [Magma_Value_Element]
-generate_fault_inputs_for_st_type' UnitT conv_func total_width
-  total_time cur_space cur_time valid cur_idx = do
+generate_fault_inputs_for_st_type' UnitT conv_func _ _ _ cur_space cur_time
+  valid cur_idx = do
   [Magma_Value_Element "" valid cur_space cur_time]
-generate_fault_inputs_for_st_type' BitT conv_func total_width
-  total_time cur_space cur_time valid cur_idx = do
+generate_fault_inputs_for_st_type' BitT conv_func _ _ _ cur_space cur_time
+  valid cur_idx = do
   [Magma_Value_Element (conv_func cur_idx Bit_For_Magma)
     valid cur_space cur_time]
-generate_fault_inputs_for_st_type' IntT conv_func total_width
-  total_time cur_space cur_time valid cur_idx = do
+generate_fault_inputs_for_st_type' IntT conv_func _ _ _ cur_space cur_time
+  valid cur_idx = do
   [Magma_Value_Element (conv_func cur_idx Int_For_Magma)
     valid cur_space cur_time]
-generate_fault_inputs_for_st_type' (ATupleT t0 t1) conv_func total_width
-  total_time cur_space cur_time valid cur_idx = do
+generate_fault_inputs_for_st_type' (ATupleT t0 t1) conv_func _ _ _ cur_space
+  cur_time valid cur_idx = do
   [Magma_Value_Element (conv_func cur_idx (ATuple_For_Magma t0 t1))
     valid cur_space cur_time]
 generate_fault_inputs_for_st_type' (STupleT n t) conv_func total_width
-  total_time cur_space cur_time valid cur_idx = do
+  total_time valid_time cur_space cur_time valid cur_idx = do
   let element_width = total_width `div` n
   let element_time = total_time
+  let element_valid_time = valid_time
   let element_strs = [generate_fault_inputs_for_st_type' t conv_func
-                      element_width element_time
+                      element_width element_time element_valid_time
                       (cur_space + j*element_width) cur_time
                       valid
-                      (cur_idx + j*element_width)
+                      (cur_idx + j*element_width*element_valid_time)
                      | j <- [0..n-1]]
   concat element_strs
 generate_fault_inputs_for_st_type' (SSeqT n t) conv_func total_width
-  total_time cur_space cur_time valid cur_idx = do
+  total_time valid_time cur_space cur_time valid cur_idx = do
   let element_width = total_width `div` n
   let element_time = total_time
-  
+  let element_valid_time = valid_time
   let element_strs = [generate_fault_inputs_for_st_type' t conv_func
-                      element_width element_time
+                      element_width element_time element_valid_time
                       (cur_space + j*element_width) cur_time
                       valid
-                      (cur_idx + j*element_width)
+                      (cur_idx + j*element_width*element_valid_time)
                      | j <- [0..n-1]]
   concat element_strs
 generate_fault_inputs_for_st_type' (TSeqT n i t) conv_func total_width
-  total_time cur_space cur_time valid cur_idx = do
+  total_time valid_time cur_space cur_time valid cur_idx = do
   let element_width = total_width
-  let element_time = total_time `div` n
+  let element_time = total_time `div` (n+i)
+  let element_valid_time = valid_time `div` n
   let element_strs = [generate_fault_inputs_for_st_type' t conv_func
-                      element_width element_time
+                      element_width element_time element_valid_time
                       cur_space (cur_time + j * element_time)
                       (valid && j < n)
-                      (cur_idx + i*element_time)
+                      (cur_idx + j*element_width*element_valid_time)
                      | j <- [0..(n+i)-1]]
   concat element_strs
--- src/Core/Aetherling/Languages/Space_Time/Deep/Types.hs
+++ src/Core/Aetherling/Languages/Space_Time/Deep/Types.hs
@@ -85,9 +85,9 @@ valid_clocks_t UnitT = 1
 valid_clocks_t BitT = 1
 valid_clocks_t IntT = 1
 valid_clocks_t (ATupleT t0 t1) = 1
-valid_clocks_t (STupleT n t) = clocks_t t
-valid_clocks_t (SSeqT n t) = clocks_t t
-valid_clocks_t (TSeqT n _ t) = n * clocks_t t
+valid_clocks_t (STupleT n t) = valid_clocks_t t
+valid_clocks_t (SSeqT n t) = valid_clocks_t t
+valid_clocks_t (TSeqT n _ t) = n * valid_clocks_t t
 
 type_to_python :: AST_Type -> String
 type_to_python UnitT = "undefined"
