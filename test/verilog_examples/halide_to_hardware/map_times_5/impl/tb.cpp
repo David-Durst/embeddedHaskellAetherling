6f72846eb07baf124256746bc2258fb048a91879
Dillon Bailey Huff <dhuff@kiwi.stanford.edu>
2019-10-14 12:04:55 -0700

    Trying to find verilated.h?

---
+++ test/verilog_examples/halide_to_hardware/map_times_5/impl/tb.cpp
@@ -0,0 +1,12 @@
+#include "Vvhls_target.h"
+
+#include "verilated.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  Vvhls_target target;
+  target.clk = 1;
+  cout << "Done" << endl;
+}
