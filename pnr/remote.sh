83df761ee2ab32e2489be0e9d908b9486eef8286
David Durst <davidbdurst@gmail.com>
2019-10-16 14:50:55 -0700

    remote and run offload more to remote server

--- pnr/remote.sh
+++ pnr/remote.sh
@@ -2,30 +2,36 @@
 
 results_temp="results/results_temp.txt"
 results="results/results.csv"
-circuit_path=$1
-circuit_name=$2
-circuit_par=$3
+circuit_dir_path=$1
 
-if [ ! -f "${results}" ]; then
-    echo "| Circuit Name | Parallelism | Total LUTs | Logic LUTs | LUTRAMs | SRLs | FFs | RAMB36 | RAMB18 | DSP48 Blocks | Slices | SLICEL | SLICEM | Slack (VIOLATED) |" > ${results_temp}
-fi
+echo "| Circuit Name | Parallelism | Total LUTs | Logic LUTs | LUTRAMs | SRLs | FFs | RAMB36 | RAMB18 | DSP48 Blocks | Slices | SLICEL | SLICEM | Slack (VIOLATED) |" > ${results_temp}
 
-./compile.sh $circuit_path constraints.xdc build
-last_build_dir=$(<last_builddir.log)
+for circuit_path in ${circuit_dir_path}/*/*/*.v; do
+	  echo "Processing ${circuit_path}"
+    circuit_basename=$(basename $circuit_path)
+    circuit_name_=`sed -n -E "s/(.*)_[^_]*/\1/p" <<< $circuit_basename`
+    circuit_name=${circuit_name_::-1}
+    circuit_par=`sed -n -E "s/.*_([^_]*).v$/\1/p" <<< $circuit_basename`
 
-echo -n "|${circuit_name}" >> ${results_temp}
-echo -n "|${circuit_par}" >> ${results_temp}
-hierarchical_results=$(sed -n -E "s/\|[^\(]top.*\(top\)\s*(.*)$/\1/p" $last_build_dir/utilization_h.txt)
-slices_results=$(sed -n -E "s/\|\s+Slice\s+\|([^\|]+\|).*$/\1/p" $last_build_dir/utilization.txt)
-slicel_results=$(sed -n -E "s/\|\s+SLICEL\s+\|([^\|]+\|).*$/\1/p" $last_build_dir/utilization.txt)
-slicem_results=$(sed -n -E "s/\|\s+SLICEM\s+\|([^\|]+\|).*$/\1/p" $last_build_dir/utilization.txt)
-timing_results=$(sed -n -E "s/^Slack.*:\s*(-?.*ns).*/\1/p" $last_build_dir/timing.txt)
-echo -n "$hierarchical_results" >> ${results_temp}
-echo -n "$slices_results" >> ${results_temp}
-echo -n "$slicel_results" >> ${results_temp}
-echo -n "$slicem_results" >> ${results_temp}
-echo -n "$timing_results" >> ${results_temp}
-echo "|" >> ${results_temp}
-tr -d [:blank:] < $results_temp >> $results
-sed -i -s 's/|/,/g' $results
-rm $results_temp
+    ./compile.sh $circuit_path constraints.xdc build
+    last_build_dir=$(<last_builddir.log)
+
+    echo -n "|${circuit_name}" >> ${results_temp}
+    echo -n "|${circuit_par}" >> ${results_temp}
+    hierarchical_results=$(sed -n -E "s/\|[^\(]top.*\(top\)\s*(.*)$/\1/p" $last_build_dir/utilization_h.txt)
+    slices_results=$(sed -n -E "s/\|\s+Slice\s+\|([^\|]+\|).*$/\1/p" $last_build_dir/utilization.txt)
+    slicel_results=$(sed -n -E "s/\|\s+SLICEL\s+\|([^\|]+\|).*$/\1/p" $last_build_dir/utilization.txt)
+    slicem_results=$(sed -n -E "s/\|\s+SLICEM\s+\|([^\|]+\|).*$/\1/p" $last_build_dir/utilization.txt)
+    timing_results=$(sed -n -E "s/^Slack.*:\s*(-?.*ns).*/\1/p" $last_build_dir/timing.txt)
+    echo -n "$hierarchical_results" >> ${results_temp}
+    echo -n "$slices_results" >> ${results_temp}
+    echo -n "$slicel_results" >> ${results_temp}
+    echo -n "$slicem_results" >> ${results_temp}
+    echo -n "$timing_results" >> ${results_temp}
+    echo "|" >> ${results_temp}
+    tr -d [:blank:] < $results_temp >> $results
+    sed -i -s 's/|/,/g' $results
+    rm $results_temp
+done
+
+rm -rf $circuit_dir_path
--- pnr/run.sh
+++ pnr/run.sh
@@ -27,16 +27,6 @@ scp $dir/remote.sh $user@kiwi:
 scp $dir/constraints.xdc $user@kiwi:
 scp $dir/compile.sh $user@kiwi:
 
-
-for circuit_path in ${dir}/../test/verilog_examples/*/*/*.v; do
-	echo "Processing ${circuit_path}"
-  scp $circuit_path $user@kiwi:
-  circuit_basename=$(basename $circuit_path)
-  circuit_name=`$sed_ -n -E "s/(.*)_[^_]*/\1/p" <<< $circuit_basename`
-  circuit_par=`$sed_ -n -E "s/.*_([^_]*).v$/\1/p" <<< $circuit_basename`
-  ssh $user@kiwi "./remote.sh $(basename $circuit_path) ${circuit_name} ${circuit_par}"
-  ssh $user@kiwi "rm $(basename $circuit_path)"
-done
-
-scp $user@kiwi:results/results.csv $dir/results.csv
+scp -r ${dir}/../test/verilog_examples $user@kiwi:
+ssh $user@kiwi "tmux new -d -s pnr './remote.sh verilog_examples/'"
 
