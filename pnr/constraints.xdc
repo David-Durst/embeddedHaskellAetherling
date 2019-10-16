4ca53c5fbcd88a10b4b49c5775abe6119e0dec19
David Durst <davidbdurst@gmail.com>
2019-10-16 13:36:48 -0700

    run works on mac, constraints and compile out of context now

--- pnr/constraints.xdc
+++ pnr/constraints.xdc
@@ -1,1 +1,2 @@
 create_clock -period 1.000 -name clk -waveform {0.000 0.500} [get_ports CLK]
+set_property HD.CLK_SRC BUFGCTRL_X0Y16 [get_ports CLK]
--- pnr/run.sh
+++ pnr/run.sh
@@ -5,6 +5,20 @@ if [ -z "$user" ]; then
     user=durst
 fi
 
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine_linux=true;;
+    Darwin*)    machine_linux=false;;
+esac
+if $machine_linux; then
+  sed_="sed"
+else
+  sed_="gsed"
+  if ! hash gsed 2>/dev/null; then
+    echo "gsed needs to be installed on mac" 1>&2
+    exit 1
+  fi
+fi
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ssh $user@kiwi "rm -rf results"
@@ -18,9 +32,8 @@ for circuit_path in ${dir}/../test/verilog_examples/*/*/*.v; do
 	echo "Processing ${circuit_path}"
   scp $circuit_path $user@kiwi:
   circuit_basename=$(basename $circuit_path)
-  circuit_name_=$(grep -Po ".*_" <<< $circuit_basename)
-  circuit_name=${circuit_name_::-1}
-  circuit_par=$(sed -n -E "s/.*_([^_]*).v$/\1/p" <<< $circuit_basename)
+  circuit_name=`$sed_ -n -E "s/(.*)_[^_]*/\1/p" <<< $circuit_basename`
+  circuit_par=`$sed_ -n -E "s/.*_([^_]*).v$/\1/p" <<< $circuit_basename`
   ssh $user@kiwi "./remote.sh $(basename $circuit_path) ${circuit_name} ${circuit_par}"
   ssh $user@kiwi "rm $(basename $circuit_path)"
 done
