a56084a1b06435442edb8e9916fcf4e0c2fddd89
David Durst <davidbdurst@gmail.com>
2019-10-18 10:14:54 -0700

    some single map leftover changes

--- pnr/remote.sh
+++ pnr/remote.sh
@@ -4,17 +4,21 @@ results_temp="results/results_temp.txt"
 results="results/results.csv"
 circuit_dir_path=$1
 
-echo "| Circuit Name | Parallelism | Total LUTs | Logic LUTs | LUTRAMs | SRLs | FFs | RAMB36 | RAMB18 | DSP48 Blocks | Slices | SLICEL | SLICEM | Slack (VIOLATED) |" > ${results_temp}
+echo "System | Application | Parallelism | Total LUTs | Logic LUTs | LUTRAMs | SRLs | FFs | RAMB36 | RAMB18 | DSP48 Blocks | Slices | SLICEL | SLICEM | Slack (VIOLATED) |" > ${results_temp}
 
 for circuit_path in ${circuit_dir_path}/*/*/*.v; do
 	  echo "Processing ${circuit_path}"
+    circuit_dir_path_length=$(expr length $circuit_dir_path)
+    circuit_path_relative=${circuit_path:$circuit_dir_path_length}
+    system=`cut -d "/" -f2 <<< <<< $circuit_path_relative`
     circuit_basename=$(basename $circuit_path)
     circuit_name=`sed -n -E "s/(.*)_[^_]*/\1/p" <<< $circuit_basename`
     circuit_par=`sed -n -E "s/.*_([^_]*).v$/\1/p" <<< $circuit_basename`
 
     ./compile.sh $circuit_path constraints.xdc build
     last_build_dir=$(<last_builddir.log)
-
+    
+    echo -n "${system}" >> ${results_temp}
     echo -n "|${circuit_name}" >> ${results_temp}
     echo -n "|${circuit_par}" >> ${results_temp}
     hierarchical_results=$(sed -n -E "s/\|[^\(]top.*\(top\)\s*(.*)$/\1/p" $last_build_dir/utilization_h.txt)
