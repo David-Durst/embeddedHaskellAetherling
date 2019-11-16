24967891355a16eaf16a8f77f8c41fe04841cf95
David Durst <davidbdurst@gmail.com>
2019-11-15 20:48:25 -0800

    cleaned up has_error

--- src/Core/Aetherling/Interpretations/Has_Error.hs
+++ src/Core/Aetherling/Interpretations/Has_Error.hs
@@ -33,31 +33,7 @@ has_error' consumer_e = do
   let f_e_maybe = get_f consumer_e
   f_error_maybe <- mapM (\f_e -> memo f_e $ has_error' f_e) f_e_maybe
   let f_error = fromMaybe False f_error_maybe
-  {-
-  let f_errorM =
-        if isJust f_e_maybe
-        then do
-          let f_e = fromJust f_e_maybe
-          memo f_e $ has_error' f_e
-        else return False
-  f_error <- f_errorM
--}
   let producers_e = get_producers consumer_e
   producer_error <- mapM (\producer_e -> memo producer_e $ has_error' producer_e)
                     producers_e
   return $ f_error || any (== True) producer_error
-  {-
-  --return $ consumer_e { seq_in = merged_producer }
-  let consumer_e_producers_replaced =
-        if length producers_e == 0
-        then consumer_e
-        else if length producers_e == 1
-        then consumer_e { seq_in = merged_producers !! 0}
-        else consumer_e {
-          seq_in_left = merged_producers !! 0,
-          seq_in_right = merged_producers !! 1
-          }
-  if isJust merged_f
-    then return $ consumer_e_producers_replaced { f = fromJust merged_f }
-    else return consumer_e_producers_replaced
--}
