b21ed0d8940052620cb352d8a490737255605d57
David Durst <davidbdurst@gmail.com>
2020-03-07 02:28:47 -0800

    checking for chisel compile fails, using .U for constants

--- src/Core/Aetherling/Interpretations/Backend_Execute/Chisel/test_chisel.sh
+++ src/Core/Aetherling/Interpretations/Backend_Execute/Chisel/test_chisel.sh
@@ -2,6 +2,11 @@ set -o xtrace
 cp $1 ${2}/src/test/scala/aetherling/modules/Top.scala
 cd $2
 sbt test:assembly
+res=$?
+if [ $res -gt 0 ]
+then
+    exit $res
+fi
 java -Xmx8G -cp target/scala-2.12/aetherling.jar org.scalatest.tools.Runner -s aetherling.modules.TopTester -t "Top should behave correctly" -o
 # sbt -mem 2048 "testOnly aetherling.modules.TopTester"
 test_result=$?
