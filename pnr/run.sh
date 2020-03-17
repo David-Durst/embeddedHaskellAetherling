cea0f9db129a1c4ed8ddf657936e035805138f7c
David Durst <davidbdurst@gmail.com>
2020-03-17 00:44:29 -0700

    removing verilog examples on each run

--- pnr/run.sh
+++ pnr/run.sh
@@ -22,6 +22,7 @@ fi
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ssh $user@kiwi "rm -rf results"
+ssh $user@kiwi "rm -rf verilog_examples"
 ssh $user@kiwi "rm -rf IP"
 ssh $user@kiwi "mkdir -p results"
 scp $dir/remote.sh $user@kiwi:
